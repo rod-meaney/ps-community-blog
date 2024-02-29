b9ef69b2368a6bdc7bd57197f353cb23a62becc2
Rod Meaney <roderick.meaney@coles.com.au>
2024-02-29 22:03:16 +1100

    Changes to simplify for blog post

--- my-support/Forms/Forms.psm1
+++ my-support/Forms/Forms.psm1
@@ -1,3 +1,1 @@
 Get-ChildItem -Path "$PSScriptRoot\*.ps1" | ForEach-Object{. $PSScriptRoot\$($_.Name)}
-Add-Type -assembly System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
--- my-utilities/GeneralUtilities/GeneralUtilities.psm1
+++ my-utilities/GeneralUtilities/GeneralUtilities.psm1
@@ -1,1 +1,3 @@
 ﻿Get-ChildItem -Path "$PSScriptRoot\*.ps1" | ForEach-Object{. $PSScriptRoot\$($_.Name)}
+Add-Type -assembly System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
