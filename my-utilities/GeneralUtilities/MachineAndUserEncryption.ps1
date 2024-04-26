de8c71bf3cde74b806cfc6ab81cb05f9d056bc97
Rod Meaney <roderick.meaney@coles.com.au>
2024-04-26 16:23:25 +1000

    Just adding some flexibility when using on systems with failover boxes

--- my-utilities/GeneralUtilities/MachineAndUserEncryption.ps1
+++ my-utilities/GeneralUtilities/MachineAndUserEncryption.ps1
@@ -22,6 +22,23 @@ Function Unprotect-WithMachineAndUserKey($s) {
 Export-ModuleMember -Function Unprotect-WithMachineAndUserKey
 # END From https://stackoverflow.com/questions/46400234/encrypt-string-with-the-machine-key-in-powershell
 
+Function Protect-WithUserKey($s) {
+    Add-Type -AssemblyName System.Security
+    $bytes = [System.Text.Encoding]::Unicode.GetBytes($s)
+    $SecureStr = [Security.Cryptography.ProtectedData]::Protect($bytes, $null, [Security.Cryptography.DataProtectionScope]::CurrentUser)
+    $SecureStrBase64 = [System.Convert]::ToBase64String($SecureStr)
+    return $SecureStrBase64
+}
+Export-ModuleMember -Function Protect-WithUserKey
+
+Function Unprotect-WithUserKey($s) {
+    Add-Type -AssemblyName System.Security
+    $SecureStr = [System.Convert]::FromBase64String($s)
+    $bytes = [Security.Cryptography.ProtectedData]::Unprotect($SecureStr, $null, [Security.Cryptography.DataProtectionScope]::CurrentUser)
+    $Password = [System.Text.Encoding]::Unicode.GetString($bytes)
+    return $Password
+}
+Export-ModuleMember -Function Unprotect-WithUserKey
 
 function Add-SecureKeyToLocalStore{
 [CmdletBinding()]
@@ -29,7 +46,8 @@ param (
     [Parameter(Mandatory=$true)] [string]$Key,
     [Parameter(Mandatory=$true)] [String]$Value,
     [Parameter(Mandatory=$true, HelpMessage="Full filename, unless using switch to make relative to MyDocuments")] [String]$StoreFileName,
-    [Parameter(Mandatory=$false)] [Switch]$UseMyDocuments
+    [Parameter(Mandatory=$false)] [Switch]$UseMyDocuments,
+    [Parameter(Mandatory=$false)] [Switch]$UserKeyOnly
 ) 
     if ($UseMyDocuments){$StoreFileName = Join-Path ([Environment]::GetFolderPath("MyDocuments")) $StoreFileName}
     if (Test-Path $StoreFileName){
@@ -39,11 +57,20 @@ param (
         Write-Host "$StoreFileName does not exist, will create"
         $keys = @{}
     }
-    if ($keys.ContainsKey($Key)){
-        $keys[$Key] = (Protect-WithMachineAndUserKey $Value)
+    if ($UserKeyOnly){
+        if ($keys.ContainsKey($Key)){
+            $keys[$Key] = (Protect-WithUserKey $Value)
+        } else {
+            $keys.Add($Key, (Protect-WithUserKey $Value))
+        }
     } else {
-        $keys.Add($Key, (Protect-WithMachineAndUserKey $Value))
+        if ($keys.ContainsKey($Key)){
+            $keys[$Key] = (Protect-WithMachineAndUserKey $Value)
+        } else {
+            $keys.Add($Key, (Protect-WithMachineAndUserKey $Value))
+        }
     }
+
     $keys | ConvertTo-Json | Set-Content $StoreFileName -Force
 }
 Export-ModuleMember -Function Add-SecureKeyToLocalStore
@@ -52,13 +79,19 @@ function Get-SecureKeyFromLocalStore{
     param (
         [Parameter(Mandatory=$true)] [string]$Key,
         [Parameter(Mandatory=$true, HelpMessage="Full filename, unless using switch to make relative to MyDocuments")] [String]$StoreFileName,
-        [Parameter(Mandatory=$false)] [Switch]$UseMyDocuments
+        [Parameter(Mandatory=$false)] [Switch]$UseMyDocuments,
+        [Parameter(Mandatory=$false)] [Switch]$UserKeyOnly
     ) 
     if ($UseMyDocuments){$StoreFileName = Join-Path ([Environment]::GetFolderPath("MyDocuments")) $StoreFileName}
     if (Test-Path $StoreFileName){
         $keys = Get-Content $StoreFileName -Raw | ConvertFrom-Json  | ConvertTo-HashtableV5
         If($keys.ContainsKey($key)) {
-            Return Unprotect-WithMachineAndUserKey $keys.$key
+            if ($UserKeyOnly){
+                Return Unprotect-WithUserKey $keys.$key
+            } else {
+                Return Unprotect-WithMachineAndUserKey $keys.$key
+            }
+            
         } else {throw "$StoreFileName does not contain $key"}
     } else {throw "$StoreFileName does not exist"}
 }
